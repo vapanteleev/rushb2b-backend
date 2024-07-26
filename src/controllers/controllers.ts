2935cf50b74e5f8b3a2710834b98b43ed2809fd6
vapanteleev <panteleevvitja32@gmail.com>
2024-07-27 00:58:45 +0300

    GetActivities

--- src/controllers/controllers.ts
+++ src/controllers/controllers.ts
@@ -5,13 +5,13 @@ import { IControllers } from './IControllers';
 
 let Controllers: IControllers = {
     async GetActivities() {
-        try {
-            const activities = await ActivityModel.find().lean();
-            return activities?.length > 0 ? activities : ['hui,pizda'];
-        } catch (error) {
-            console.error('Error fetching activities:', error);
-            return ['hui,pizda'];
-        }
+        // try {
+        // const activities = await ActivityModel.find().lean();
+        // return activities?.length > 0 ? activities : ['hui,pizda'];
+        // } catch (error) {
+        // console.error('Error fetching activities:', error);
+        return ['hui,pizda'];
+        // }
     }
 }
 export { Controllers }
